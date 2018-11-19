b5ca1f9810e4da64b2a5c40fe77062a398c53258
Marcus Wilkinson <marcuswilkinson@deltek.com>
2018-11-19 14:29:07 +0000

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListItemComponent } from './list-item/list-item.component';
+import { ListComponent } from './list/list.component';
+import { DetailComponent } from './detail/detail.component';
+import { ChatWindowComponent } from './chat-window/chat-window.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListItemComponent,
+    ListComponent,
+    DetailComponent,
+    ChatWindowComponent
   ],
   imports: [
     BrowserModule,
